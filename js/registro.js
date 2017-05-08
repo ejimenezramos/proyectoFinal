c34670eda36e1d9164933b31a001dce45f89bd98
ejimenezramos <ejimenezramos@hotmail.com>
2017-05-08 18:00:39 +0200

    170508 cambios register

--- js/registro.js
+++ js/registro.js
@@ -1,10 +1,26 @@
 /**
  * Created by elena.jimenez on 08/05/2017.
  */
+function validate() {
+    return checkDNI();
+}
+function checkDNI() {
+    if (document.getElementById("dni").value == "5555") {
+        alert("hola");
+        return true;
+    }
+    else {
+        alert("no");
+        return false;
+    }
 
+}
+function checkValidity(){
+    document.getElementById("registro");
 
-$("registro").submit(function () {
-  var dni=$("registro").find($("dni"));
-    alert(dni);
-})
-;
+}
+/*$("registro").submit(function () {
+ var dni=$("registro").find($("dni"));
+ alert(dni);
+ })
+ ;*/
