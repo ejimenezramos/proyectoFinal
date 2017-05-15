3b3cc8817bd4d8e7232477000fa4fd6a4696270f
Elena Jiménez <ejimenezramos@hotmail.com>
2017-05-15 22:01:43 +0200

    pruebacommit

---
+++ js/jquery.peticionesAJAX.js
@@ -0,0 +1,37 @@
+/**
+ * Created by elena.jimenez on 03/05/2017.
+ */
+/****PETICIÓN AJAX PARA EL FILTRO POR CIUDAD REGISTRO CLIENTE****/
+/*Elena:petición AJAX para el filtro de provincias y municipios en la página registro*/
+$(document).ready(function(){
+    $("#comunidades").change(function(){
+        if($("#comunidades").val()==0){
+           $("#errorcomunidades").html('Por favor seleccione una Comunidad Autónoma de la lista');
+            $("#provincias").attr('disabled', true);
+            $("#tipoDireccion").attr('disabled', true);
+            $("#direccion").attr('disabled', true);
+            $("#cp").attr('disabled', true);
+        }else {
+            $("#errorcomunidades").html('');
+            $.ajax({
+                method: "POST",
+                url: "./requests/peticiones.php",
+                data: {codigoComunidad: $("#comunidades").val()}
+            })
+                .success(function (result) {
+                    $("#provincias").attr('disabled', false);
+
+                   var ob=JSON.parse(result);
+                    $("#provincias").html("<option value='0'>--Seleccione una provincia--</option>");
+                    for (i=0;i<ob.provincia.length;i++){
+                        $("#provincias").append("<option value="+ob.provincia[i]+">"+ob.provincia[i]+"</option>");
+
+                    }
+
+                })
+                .error(function (error) {
+                    console.log(error);
+                });
+        }
+    });
+});
