7fea372e3db36faa2690ef04dbd4744a80a2a1c6
pablo barrantes <pablo.barrantes@aselcis.com>
2017-05-24 13:22:04 +0200

    Pablo: cambios en el footer

--- mvc/vista/home.php
+++ mvc/vista/home.php
@@ -101,10 +101,8 @@ function hideURLbar(){ window.scrollTo(0,1); }
 	<script type="text/javascript">
 		$(window).load(function(){
 			$(window).scroll(function() {
-				var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
-				console.log(wintop);
+				var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();	
 				var totalScroll = (wintop/(docheight-winheight))*100;
-				console.log("total scroll" + totalScroll);
 				$(".KW_progressBar").css("width",totalScroll+"%");
 			});
 		});
@@ -297,11 +295,14 @@ function hideURLbar(){ window.scrollTo(0,1); }
 			</h3>
 
 			<p class="footer-links">
-				<a class="cursor_pointer"  onclick="window.open('?pagina=licencia', 'Licencia', 'width=900, height=600')"> Licencia</a> Â· 
-				<a data-toggle="modal" data-target="#ModalDevoluciones">Devoluciones</a><br>
-
-				<a href="?pagina=packs">EnvÃ­o y entrega</a> Â· <a
-					href="?pagina=packs">Preguntas frecuentes</a>
+				<a class="cursor_pointer"
+					onclick="window.open('?pagina=licencia', 'Licencia', 'width=900, height=600')">
+					Licencia</a> Â· <a class="cursor_pointer" data-toggle="modal"
+					data-target="#ModalDevoluciones">Devoluciones</a><br> <a
+					class="cursor_pointer" data-toggle="modal"
+					data-target="#ModalEnvioEntrega">EnvÃ­o y entrega</a> Â· <a
+					data-toggle="modal" data-target="#ModalPreguntas">Preguntas
+					frecuentes</a>
 			</p>
 
 			<p class="footer-company-name">Cereal Break &copy; 2017</p>
@@ -355,6 +356,164 @@ function hideURLbar(){ window.scrollTo(0,1); }
 	</footer>
 
 	<!-- //footer-->
+
+	<!-- MODALES DEL FOOTER -->
+	<!-- Pablo 24/05/17 Modal para las Devoluciones -->
+
+	<div class="modal fade" id="ModalDevoluciones" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Pol&iacute;tica de devoluci&oacute;n de
+						productos</h4>
+				</div>
+				<div class="modal-body">
+					<p>
+						Si usted no queda completamente satisfecho con el producto
+						adquirido, se le repondr&aacute; la pieza o devolver el dinero
+						siempre que cumpla con la las especificaciones detalladas a
+						continuaci&oacuten. <br>Para ello es necesario que los productos
+						(cereales, leche o topping) conserven los embalajes y precintos
+						originales. Importante: no abrir el producto si se tiene la
+						intenci&oacute;n de cambiar o devolverlo <br>En el caso de que
+						desee devolver o cambiar un producto deber&aacute; enviarnos un
+						correo electr&oacutenico a la direcci&oacuten
+						support@cerealbreak.com explicando el motivo del cambio, indicando
+						el n&uacute;mero de pedido y la fecha de recepci&oacute;n y
+						adjuntando una foto del producto o productos en el estado en el
+						que encuentran para su devoluci&oacute;n o cambio. <br>Para
+						proceder al cambio o devoluci&oacute;n de un producto:
+					</p>
+					<ol>
+						<li class="list-group-item1">El usuario o cliente dispone de siete
+							d&iacute;as h&aacutebiles, contados desde la recepci&oacute;n del
+							producto, para desistir del contrato sin necesidad de
+							indicaci&oacute;n de los motivos siempre que el producto
+							est&eacute; en perfectas condiciones</li>
+						<li class="list-group-item1">Conservar el envoltorio original si
+							&eacute;ste est&aacute; abierto no se podr&aacute; proceder al
+							cambio o devoluci&oacute;n.</li>
+						<li class="list-group-item1">Es requisito imprescindible que tanto
+							el embalaje como el producto est&eacute;n en perfecto estado, no
+							hayan sido manipulados, no presenten defectos ni deterioros por
+							hechos externos u otras circunstancias que deriven en el
+							menoscabo del producto, en especial la apertura del recipiente.</li>
+						<li class="list-group-item1">Cualquier producto abierto, cortado o
+							manipulado de cualquier manera no podr&aacute; ser sujeto a
+							cambio o devoluci&oacute;n.</li>
+						<li class="list-group-item1">El pedido devuelto deber&aacute; ser
+							entregado junto con el albar&aacute;n de entrega y factura a la
+							direcci&oacute;n que se le indicar&aacute; una vez se haya valora
+							el caso y se proceda al cambio o devoluci&oacute;n.</li>
+					</ol>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Pablo 24/05/17 Modal para el Envio y Entrega de los productos-->
+	<div class="modal fade" id="ModalEnvioEntrega" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+				<div class="modal-body">
+					<div class="wpb_wrapper">
+						<h5>
+							<span>TARIFAS Y CONDICIONES DEL TRANSPORTE</span>
+						</h5>
+						<hr>
+						<p>
+							<span>Realizamos todos nuestros pedidos en las 48h tras el pedido
+								mediante las empresas&nbsp;<strong>NACEX</strong>, <strong>TNT</strong>
+								y <strong>CORREOS&nbsp;</strong>
+							</span>
+						</p>
+						<p>
+							<span>&nbsp;</span>
+						</p>
+						<p>
+							<span>Las <strong>TARIFAS DE ENV&#205;O</strong>&nbsp;van desde <strong>3,05&#8364;</strong>&nbsp;dependiendo
+								del destino y peso del pedido.
+							</span>
+						</p>
+						<p>
+							<span>&nbsp;</span>
+						</p>
+						<p>
+							<span>1. Las presentes condiciones ser&aacute;n de
+								aplicaci&oacute;n para los env&iacute;os contratados en defecto
+								de pactos o condiciones espec&iacute;ficas derivadas de la
+								singularidad de cada producto o servicio.</span>
+						</p>
+						<p>
+							<span>2. Los datos sobre el destino del pedido y para el
+								destinatario, son facilitados por &eacute;ste y bajo su
+								responsabilidad, no siendo comprobados por la agencia de
+								transporte, salvo grave riesgo inminente u orden de la
+								autoridad.</span>
+						</p>
+						<p>
+							<span>3. La prestaci&oacute;n de los servicios se
+								efectuar&aacute; de lunes a viernes. En el c&oacute;mputo
+								temporal se excluyen los d&iacute;as festivos en la localidad de
+								destino.</span>
+						</p>
+						<p>
+							<span>4. En caso de incumplimiento del plazo de entrega
+								contractualmente pactado, la indemnizaci&oacute;n a abonar
+								consistir&aacute; en la devoluci&oacute;n de los portes
+								satisfechos por el cliente. No se considerar&aacute;
+								incumplimiento del plazo de entrega aquellos causados por
+								ausencia del destinatario, cambio del domicilio o
+								direcci&oacute;n desconocidas, entregas fuera de plazo en
+								segundo intento de entrega, causas de fuerza mayor o caso
+								fortuito u otras no imputables a la agencia de transporte. En el
+								caso de que el pedido sea rechazado por el destinatario o el
+								destinatario se halle ausente, Cereal Break&nbsp;no asume
+								ninguna responsabilidad y se reserva el derecho a cobrar al
+								cliente el 100% del valor del pedido.</span>
+						</p>
+						<p>
+							<span>5. La entrega del pedido se realizar&aacute; en el
+								domicilio designado por el destinatario, salvo por reiterada
+								ausencia del destinatario, en cuyo caso, se efectuar&aacute; en
+								la central m&aacute;s cercana del destinatario. En caso de
+								ausencia del destinatario se dejar&aacute; aviso del intento de
+								entrega, as&iacute; como de la forma de acordar la misma.</span>
+						</p>
+						<p>
+							<span>6. Ser&aacute; requisito necesario para proceder a la
+								entrega de la mercanc&iacute;a la firma del pedido del
+								destinatario. En caso de negativa del destinatario a firmar el
+								pedido de la agencia quedar&aacute; eximida de toda
+								responsabilidad.</span>
+						</p>
+						<p>
+							<span>7. Los plazos para efectuar la reclamaci&oacute;n por
+								da&ntildeos en la mercanc&iacute;a ser&aacute;n de 24 horas
+								desde la entrega si los da&ntildeos no son apreciables
+								exteriormente.</span>
+						</p>
+						<p>
+							<span>&nbsp;</span>
+						</p>
+						<p>
+							<span><strong>CEREAL BREAK</strong><br> </span></span>
+						</p>
+
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<!-- smooth scrolling -->
 	<script type="text/javascript">
@@ -423,33 +582,9 @@ function hideURLbar(){ window.scrollTo(0,1); }
 
 		});
 	</script>
-	
-	<div class="modal fade" id="ModalDevoluciones" tabindex="-1" role="dialog"
-		aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content modal-info">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal"
-						aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div>
-					<h3>Política de devolución de productos</h3>
-					<p class="quick_desc">Si usted no queda completamente satisfecho con el producto adquirido, se le repondrá la pieza o devolverá el dinero siempre que cumpla con la las especificaciones detalladas a continuación.
-					<br>Para ello es necesario que los productos (cereales, leche o topping) conserven los embalajes y precintos originales. Importante: no abrir el producto si se tiene la intención de cambiar o devolverlo
-					<br>En el caso de que desee devolver o cambiar un producto deberá enviarnos un correo electrónico a la dirección support@cerealbreak.com explicando el motivo del cambio, indicando el número de pedido y la fecha de recepción y adjuntando una foto del producto o productos en el estado en el que encuentran para su devolución o cambio.
-					<br>Para proceder al cambio o devolución de un producto:</p>
-					<dl>
-						<dd>El usuario o cliente dispone de siete días hábiles, contados desde la recepción del producto, para desistir del contrato sin necesidad de indicación de los motivos siempre que el producto esté en perfectas condiciones</dd>
-						<dd>Conservar el envoltorio original; si éste está abierto no se podrá proceder al cambio o devolución.</dd>
-						<dd>Es requisito imprescindible que tanto el embalaje como el producto estén en perfecto estado, no hayan sido manipulados, no presenten defectos ni deterioros por hechos externos u otras circunstancias que deriven en el menoscabo del producto, en especial la apertura del recipiente.</dd>
-						<dd>Cualquier producto abierto, cortado o manipulado de cualquier manera no podrá ser sujeto a cambio o devolución.</dd>
-						<dd>El pedido devuelto deberá ser entregado junto con el albarán de entrega y factura a la dirección que SuperJamon.Es le indicará una vez se haya valora el caso y se proceda al cambio o devolución.</dd>
-					</dl>
-				</div>
-			</div>
-		</div>
-	</div>
+
+
+
+
 </body>
 </html>
--- mvc/vista/register.php
+++ mvc/vista/register.php
@@ -76,16 +76,14 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <body>
 <!--Pablo 21/05/17: Barra de progreso-->
 <script type="text/javascript">
-    $(window).load(function(){
-        $(window).scroll(function() {
-            var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
-            console.log(wintop);
-            var totalScroll = (wintop/(docheight-winheight))*100;
-            console.log("total scroll" + totalScroll);
-            $(".KW_progressBar").css("width",totalScroll+"%");
-        });
-    });
-</script>
+		$(window).load(function(){
+			$(window).scroll(function() {
+				var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();	
+				var totalScroll = (wintop/(docheight-winheight))*100;
+				$(".KW_progressBar").css("width",totalScroll+"%");
+			});
+		});
+	</script>
 <div class="KW_progressContainer">
     <div class="KW_progressBar"></div>
 </div>
@@ -303,14 +301,15 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <h3>Cereal<span>Break</span></h3>
 
         <p class="footer-links">
-            <a href="?pagina=home">Licencia</a>
-            ·
-            <a href="?pagina=productos">Devoluciones</a><br>
-
-            <a href="?pagina=packs">Envío y entrega</a>
-            ·
-            <a href="?pagina=packs">Preguntas frecuentes</a>
-        </p>
+				<a class="cursor_pointer"
+					onclick="window.open('?pagina=licencia', 'Licencia', 'width=900, height=600')">
+					Licencia</a> · <a class="cursor_pointer" data-toggle="modal"
+					data-target="#ModalDevoluciones">Devoluciones</a><br> <a
+					class="cursor_pointer" data-toggle="modal"
+					data-target="#ModalEnvioEntrega">Envío y entrega</a> · <a
+					data-toggle="modal" data-target="#ModalPreguntas">Preguntas
+					frecuentes</a>
+			</p>
 
         <p class="footer-company-name">Cereal Break &copy; 2017</p>
     </div>
@@ -420,6 +419,165 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
     });
 </script>
+
+<!-- MODALES DEL FOOTER -->
+	<!-- Pablo 24/05/17 Modal para las Devoluciones -->
+
+	<div class="modal fade" id="ModalDevoluciones" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Pol&iacute;tica de devoluci&oacute;n de
+						productos</h4>
+				</div>
+				<div class="modal-body">
+					<p>
+						Si usted no queda completamente satisfecho con el producto
+						adquirido, se le repondr&aacute; la pieza o devolver el dinero
+						siempre que cumpla con la las especificaciones detalladas a
+						continuaci&oacuten. <br>Para ello es necesario que los productos
+						(cereales, leche o topping) conserven los embalajes y precintos
+						originales. Importante: no abrir el producto si se tiene la
+						intenci&oacute;n de cambiar o devolverlo <br>En el caso de que
+						desee devolver o cambiar un producto deber&aacute; enviarnos un
+						correo electr&oacutenico a la direcci&oacuten
+						support@cerealbreak.com explicando el motivo del cambio, indicando
+						el n&uacute;mero de pedido y la fecha de recepci&oacute;n y
+						adjuntando una foto del producto o productos en el estado en el
+						que encuentran para su devoluci&oacute;n o cambio. <br>Para
+						proceder al cambio o devoluci&oacute;n de un producto:
+					</p>
+					<ol>
+						<li class="list-group-item1">El usuario o cliente dispone de siete
+							d&iacute;as h&aacutebiles, contados desde la recepci&oacute;n del
+							producto, para desistir del contrato sin necesidad de
+							indicaci&oacute;n de los motivos siempre que el producto
+							est&eacute; en perfectas condiciones</li>
+						<li class="list-group-item1">Conservar el envoltorio original si
+							&eacute;ste est&aacute; abierto no se podr&aacute; proceder al
+							cambio o devoluci&oacute;n.</li>
+						<li class="list-group-item1">Es requisito imprescindible que tanto
+							el embalaje como el producto est&eacute;n en perfecto estado, no
+							hayan sido manipulados, no presenten defectos ni deterioros por
+							hechos externos u otras circunstancias que deriven en el
+							menoscabo del producto, en especial la apertura del recipiente.</li>
+						<li class="list-group-item1">Cualquier producto abierto, cortado o
+							manipulado de cualquier manera no podr&aacute; ser sujeto a
+							cambio o devoluci&oacute;n.</li>
+						<li class="list-group-item1">El pedido devuelto deber&aacute; ser
+							entregado junto con el albar&aacute;n de entrega y factura a la
+							direcci&oacute;n que se le indicar&aacute; una vez se haya valora
+							el caso y se proceda al cambio o devoluci&oacute;n.</li>
+					</ol>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- Pablo 24/05/17 Modal para el Envio y Entrega de los productos-->
+	<div class="modal fade" id="ModalEnvioEntrega" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+				<div class="modal-body">
+					<div class="wpb_wrapper">
+						<h5>
+							<span>TARIFAS Y CONDICIONES DEL TRANSPORTE</span>
+						</h5>
+						<hr>
+						<p>
+							<span>Realizamos todos nuestros pedidos en las 48h tras el pedido
+								mediante las empresas&nbsp;<strong>NACEX</strong>, <strong>TNT</strong>
+								y <strong>CORREOS&nbsp;</strong>
+							</span>
+						</p>
+						<p>
+							<span>&nbsp;</span>
+						</p>
+						<p>
+							<span>Las <strong>TARIFAS DE ENV&#205;O</strong>&nbsp;van desde <strong>3,05&#8364;</strong>&nbsp;dependiendo
+								del destino y peso del pedido.
+							</span>
+						</p>
+						<p>
+							<span>&nbsp;</span>
+						</p>
+						<p>
+							<span>1. Las presentes condiciones ser&aacute;n de
+								aplicaci&oacute;n para los env&iacute;os contratados en defecto
+								de pactos o condiciones espec&iacute;ficas derivadas de la
+								singularidad de cada producto o servicio.</span>
+						</p>
+						<p>
+							<span>2. Los datos sobre el destino del pedido y para el
+								destinatario, son facilitados por &eacute;ste y bajo su
+								responsabilidad, no siendo comprobados por la agencia de
+								transporte, salvo grave riesgo inminente u orden de la
+								autoridad.</span>
+						</p>
+						<p>
+							<span>3. La prestaci&oacute;n de los servicios se
+								efectuar&aacute; de lunes a viernes. En el c&oacute;mputo
+								temporal se excluyen los d&iacute;as festivos en la localidad de
+								destino.</span>
+						</p>
+						<p>
+							<span>4. En caso de incumplimiento del plazo de entrega
+								contractualmente pactado, la indemnizaci&oacute;n a abonar
+								consistir&aacute; en la devoluci&oacute;n de los portes
+								satisfechos por el cliente. No se considerar&aacute;
+								incumplimiento del plazo de entrega aquellos causados por
+								ausencia del destinatario, cambio del domicilio o
+								direcci&oacute;n desconocidas, entregas fuera de plazo en
+								segundo intento de entrega, causas de fuerza mayor o caso
+								fortuito u otras no imputables a la agencia de transporte. En el
+								caso de que el pedido sea rechazado por el destinatario o el
+								destinatario se halle ausente, Cereal Break&nbsp;no asume
+								ninguna responsabilidad y se reserva el derecho a cobrar al
+								cliente el 100% del valor del pedido.</span>
+						</p>
+						<p>
+							<span>5. La entrega del pedido se realizar&aacute; en el
+								domicilio designado por el destinatario, salvo por reiterada
+								ausencia del destinatario, en cuyo caso, se efectuar&aacute; en
+								la central m&aacute;s cercana del destinatario. En caso de
+								ausencia del destinatario se dejar&aacute; aviso del intento de
+								entrega, as&iacute; como de la forma de acordar la misma.</span>
+						</p>
+						<p>
+							<span>6. Ser&aacute; requisito necesario para proceder a la
+								entrega de la mercanc&iacute;a la firma del pedido del
+								destinatario. En caso de negativa del destinatario a firmar el
+								pedido de la agencia quedar&aacute; eximida de toda
+								responsabilidad.</span>
+						</p>
+						<p>
+							<span>7. Los plazos para efectuar la reclamaci&oacute;n por
+								da&ntildeos en la mercanc&iacute;a ser&aacute;n de 24 horas
+								desde la entrega si los da&ntildeos no son apreciables
+								exteriormente.</span>
+						</p>
+						<p>
+							<span>&nbsp;</span>
+						</p>
+						<p>
+							<span><strong>CEREAL BREAK</strong><br> </span></span>
+						</p>
+
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html>
