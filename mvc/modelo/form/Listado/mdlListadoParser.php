d7db0f2071e89206aba5758b6d6c08c0ff765ed5
pablo barrantes <pablo.barrantes@aselcis.com>
2017-06-01 11:01:28 +0200

    Pablo: cambios en los modales

--- mvc/modelo/form/Listado/mdlListadoParser.php
+++ mvc/modelo/form/Listado/mdlListadoParser.php
@@ -39,7 +39,7 @@ class ListadoParser {
 												</div>
 												<div class='add'>
 														<button class='btn btn-danger my-cart-btn my-cart-b'
-	                                						data-id='" .$productos['Foto']. "' data-name='Rice' data-summary='summary" .$productos['Foto']. "'
+	                                						data-id='" .$productos['Foto']. "' data-name='" . $productos['Nombre_Prod'] . "' data-summary='summary" .$productos['Foto']. "'
 															data-price='" . $productos['Precio'] . "' data-quantity='1'
 															data-image='images/" .$productos['Foto']. ".png'>Add to Cart</button>
 												</div>
@@ -64,31 +64,4 @@ class ListadoParser {
     }
 
 }
-/*<div class="col-md-3 pro-1">
-<div class="col-m">
-<a href="#" data-toggle="modal" data-target="#myModal4"
-		class="offer-img"> <img src="images/boxfroot.png"
-				class="img-responsive" alt="">
-				</a>
-				<div class="mid-1">
-				<div class="women">
-				<h6>
-				<a href="single.html">Froot Loops</a>
-				</h6>
-				</div>
-				<div class="mid-2">
-				<p>
-				<label>3.50€</label><em class="item_price">3.20€</em>
-				</p>
-				<div class="block"></div>
-				<div class="clearfix"></div>
-				</div>
-				<div class="add">
-				<button class="btn btn-danger my-cart-btn my-cart-b"
-						data-id="27" data-name="Rice" data-summary="summary 27"
-								data-price="3.20" data-quantity="1"
-										data-image="images/boxfroot.png">Add to Cart</button>
-										</div>
-										</div>
-										</div>
-										</div>*/
+
