78c4bdec59afc3af060c83d85849b58981b638e3
pablo barrantes <pablo.barrantes@aselcis.com>
2017-06-05 12:19:08 +0200

    Pablo: registro ok, vista user

---
+++ mvc/modelo/form/User/mdlUser.php
@@ -0,0 +1,48 @@
+<?php
+
+class mdlUser extends Padre {
+
+    const PAGE = 'user';
+
+    public function onGestionPagina() {
+        if (getGet('pagina') != self::PAGE)
+            return;
+// Validamos
+        $val = Validacion::getInstance();
+// Validamos los elementos que hay en $_POST
+        $toValidate = ($_POST);
+        $rules = array(
+            'id' => 'required');
+        $id = getPost('id');
+        $val->addRules($rules);
+        $val->run($toValidate);
+        if (!is_null(getPost(self::PAGE))) {
+            if ($val->isValid()) {
+// Guardamos los datos en session
+                $_SESSION[self::PAGE] = $val->getOks();
+                $_SESSION['datos'] = productos::searchIdDB($id);
+// Cambiamos el paso
+                redirectTo('index.php?pagina=user');
+            }
+        }
+
+    }
+
+
+    public function onCargarVista($path) {
+        if (getGet('pagina') != self::PAGE)
+            return;
+        if(isset($_GET['cerrar']))
+        {
+            session_destroy();
+        }
+        ob_start();
+        include $path;
+        $vista = ob_get_contents();
+        ob_end_clean();
+
+        echo UserParser::loadContent($vista);
+    }
+
+}
+
